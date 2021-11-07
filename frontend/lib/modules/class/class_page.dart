bcaa538041b49d5e1d4d7f8faf005d30cc150158
celinalind <celina.lind@novvia.com>
2021-11-07 00:48:43 -0400

    class page update

--- frontend/lib/modules/class/class_page.dart
+++ frontend/lib/modules/class/class_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:expandable/expandable.dart';
 
 class ClassPage extends StatefulWidget {
   const ClassPage({Key? key, required this.title}) : super(key: key);
@@ -11,59 +12,93 @@ class ClassPage extends StatefulWidget {
 
 class _ClassPageState extends State<ClassPage> {
   final _formKey = GlobalKey<FormState>();
-  List<int> noteIdList = [1, 2, 3, 4, 5];
+  List<int> noteIdList = [1, 2, 3, 4, 5, 6, 7, 8, 6, 5, 4, 3, 33, 2, 2, 3];
   String topicName = 'Expensive';
   String className = 'Equation Time';
+  List<int> topicIdList = [1, 3, 2, 4, 5, 6, 4, 2];
 
-  Widget imageList(int noteId, Size screenSize) {
-    // List<Widget> noteList = [];
-    // noteIds.forEach((id) {
-    //   noteList.add(Container(
-    //     width: screenSize.width > 400 ? screenSize.width / 2 : screenSize.width,
-    //     color: Colors.lightGreenAccent,
-    // ));
-    // });
-    // return noteList;
-    return Container(
-      width: screenSize.width > 400 ? screenSize.width / 2 : screenSize.width,
-      color: Colors.lightGreenAccent,
-    );
+  List<Widget> myTopics(List<int> noteIds, Size screenSize, List<int> topicIds, String topicName) {
+    List<Widget> topicList = [];
+
+    topicIds.forEach((id) {
+      topicList.add(Container(
+          child: ExpandablePanel(
+        header: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            topicName,
+            style: TextStyle(fontSize: 24, color: Color(0xff133c55)),
+          ),
+        ),
+        collapsed: SizedBox(height: 10),
+        expanded: Column(
+          children: [
+            PreferredSize(
+                preferredSize: const Size.fromHeight(200),
+                child: screenSize.width > 370
+                    ? Wrap(spacing: 8.0, runSpacing: 4.0, children: myNotes(noteIds, screenSize))
+                    : SingleChildScrollView(scrollDirection: Axis.horizontal, child: Row(children: myNotes(noteIds, screenSize)))),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                child: Center(
+                    child: Column(children: const [
+                  Text('Upload New Note'),
+                  Icon(
+                    Icons.add_a_photo,
+                    size: 24,
+                  )
+                ])),
+                onPressed: () {
+                  // getImage(ImageSource.gallery);
+                },
+              ),
+            ),
+          ],
+        ),
+      )));
+    });
+    return topicList;
+  }
+
+  List<Widget> myNotes(List<int> noteIds, Size screenSize) {
+    List<Widget> noteList = [];
+    noteIds.forEach((id) {
+      noteList.add(Container(
+          height: 150,
+          width: 110,
+          child: ListTile(
+              title: Container(
+            color: Colors.lightGreenAccent,
+          ))));
+    });
+    return noteList;
   }
 
   @override
   Widget build(BuildContext context) {
     Size screenSize = MediaQuery.of(context).size;
-    debugPrint('ScreenSizing:' + screenSize.toString());
-    debugPrint('NOTE ID LENGTH: ' + noteIdList.length.toString());
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
-        ListTile(
-            title: Text(className, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 35)),
-            subtitle: ListView(children: [
-              Card(
-                  child: ListTile(
-                title: Text(topicName),
-                // subtitle: Container(
-                //   width: screenSize.width > 400 ? screenSize.width / 2 : screenSize.width,
-                //   color: Colors.lightGreenAccent,
-                // ),
-                subtitle: Row(children: [
-                  ListView.builder(
-                    itemCount: noteIdList.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return imageList(noteIdList[index], screenSize);
-                    },
-                    scrollDirection: Axis.horizontal,
-                  )
-                ]),
-              ))
-            ])),
-      ]),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+            child: Column(
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Classes:',
+              style: TextStyle(color: Color(0xff133c55), fontSize: 23),
+            ),
+            Column(children: myTopics(noteIdList, screenSize, topicIdList, topicName)),
+          ],
+        )),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           showDialog(
@@ -79,7 +114,7 @@ class _ClassPageState extends State<ClassPage> {
                         onTap: () {
                           Navigator.of(context).pop();
                         },
-                        child: CircleAvatar(
+                        child: const CircleAvatar(
                           child: Icon(Icons.close),
                           backgroundColor: Colors.red,
                         ),
@@ -91,11 +126,11 @@ class _ClassPageState extends State<ClassPage> {
                         mainAxisSize: MainAxisSize.min,
                         children: <Widget>[
                           Padding(
-                            padding: EdgeInsets.all(8.0),
+                            padding: const EdgeInsets.all(8.0),
                             child: ListTile(title: const Text('Enter Topic Name'), subtitle: TextFormField()),
                           ),
                           Padding(
-                            padding: EdgeInsets.all(8.0),
+                            padding: const EdgeInsets.all(8.0),
                             child: TextButton(
                               child: Center(
                                   child: Column(children: const [
@@ -112,8 +147,8 @@ class _ClassPageState extends State<ClassPage> {
                           ),
                           Padding(
                             padding: const EdgeInsets.all(8.0),
-                            child: RaisedButton(
-                              child: Text("Create Topic"),
+                            child: ElevatedButton(
+                              child: const Text("Create Topic"),
                               onPressed: () {
                                 if (_formKey.currentState!.validate()) {
                                   _formKey.currentState!.save();
